7f48b21b5bb97f8a74db1f8bcf28318939d0c944
Cleecanth <Chris@pa-wf-250-38.local>
2014-02-21 17:46:33 -0600

    Added tab support
    - Created tab stylings.
    
    - Included tabs that transform into accordions in plugins.js and
    components/_tabs.scss
    
    - Changed @mixin grid-media-query to more general media-query.
    
    - Fixed some color variable issues.
    
    - Moved grid mixins out to extend.scss
    
    - Added some Windows 8 media queries for better snap-view support.

--- _partials/js/plugins.js
+++ _partials/js/plugins.js
@@ -21,4 +21,76 @@
     }
 }());
 
+//Tabs to accordions
+(function(a) {
+    a.accordionTabs = function(b) {
+        var c = this;
+        var tabGroups = [];
+        var z = a(".accordion_tab");
+
+        c.init = function() {
+            for (var i=0; i < z.length; i++){
+                var y = z[i];
+                var x = a(y).next()[0];
+                var w = a(y).next().next();
+
+                tabGroups.push(y,x);
+
+                if (a(w).hasClass("accordion_tab") === false ){
+                    var addons = a(y).attr('class');
+                    a(tabGroups).wrapAll("<section class = 'tabbed "+addons+"' />");
+                    tabGroups = [];
+                }
+            };
+
+            a('.tabbed').removeClass('accordion_tab').prepend("<nav class='tabs' />");
+            a(".accordion_tab").each(function(){
+                var tabNav = a(this).parent(".tabbed").children(".tabs");
+                a(this).clone().appendTo(tabNav).addClass('tab').removeClass("accordion_tab");
+            });
+
+            a(".tabbed .accordion_tab:first-of-type, .tabbed .tab:first-child").addClass("active");
+            a(".tab_content").hide();
+            var d = ""+a(".tabbed .accordion_tab:first-of-type").map(function(){return a(this).attr("href");}).get();
+            a(d).each(function(){a(this).show()});
+
+            var sideHeight = a('.side_tabs .tabs').outerHeight();
+            a('.side_tabs').find('.tab_content').css("min-height", sideHeight);
+
+            c.options = a.extend({}, a.accordionTabs.defaultOptions, b);
+
+            a(".tabs .tab").click(function(e) {
+                if (a(this).hasClass('active') === false){
+                    var d = a(this).attr("href");
+                    a(this).closest('.tabbed').find(".tab_content:first").siblings(".tab_content").andSelf().hide();
+                    a(d).fadeIn(c.options.speed);
+                    a(this).closest('.tabbed').find(".tab:first, .accordion_tab:first").siblings(".tab,.accordion_tab").andSelf().removeClass("active");
+                    a(this).addClass("active");
+                    a(this).closest('.tabbed').find(".accordion_tab[href^='" + d + "']").addClass("active");
+                }
+                e.preventDefault();
+            });
+            a(".accordion_tab").click(function(e) {
+                if (a(this).hasClass('active') === false){
+                    var d = a(this).attr("href");
+                    a(this).closest('.tabbed').find(".tab_content:first").siblings(".tab_content").andSelf().slideUp('fast');
+                    a(d).css("min-height","0").slideDown(c.options.speed);
+                    a(this).closest('.tabbed').find(".tab:first, .accordion_tab:first").siblings(".tab, .accordion_tab").andSelf().removeClass("active");
+                    a(this).addClass("active");
+                    a(this).closest('.tabbed').find(".tab[href^='" + d + "']").addClass("active");
+                }
+                e.preventDefault();
+            })
+
+        };
+        c.init()
+    };
+    a.accordionTabs.defaultOptions = {speed: 300};
+    a.fn.accordionTabs = function(b) {
+        return this.each(function() {
+            (new a.accordionTabs(b))
+        })
+    }
+})(jQuery);
+
 // Place any jQuery/helper plugins in here.
