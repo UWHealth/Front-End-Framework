a4c27529c2ef1c7702bae594a85f6463beccf7bd
Cleecanth <Chris@pa-wf-250-38.local>
2014-03-03 15:04:14 -0600

    Made tabs more modular
    - Rearranged folder structure.

--- _partials/js/plugins.js
+++ _partials/js/plugins.js
@@ -192,4 +192,25 @@
     }
 })(jQuery);
 
+
+//Responsive tables
+(function(t){
+
+    var all_tables = t("table");
+    var y = [];
+    var z;
+    for(i=0; i<all_tables.length; i++){
+        var current = t(all_tables)[i];
+        z = t(current).find("th").map(function(){t(this).each(function(){return this.innerText})});
+        y.push(z.prevObject);
+
+        y = y[0];
+        t(current).find("tr").each(function(){
+            t(this).find("td").each(function(j){
+                t(this).attr("data-th",y[j].innerText);
+            });
+        });
+    };
+
+})(jQuery);
 // Place any jQuery/helper plugins in here.
