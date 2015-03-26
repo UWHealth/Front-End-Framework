365017cef9543cd011a2c4fae254524f39ef31e1
Cleecanth <Chris@pa-wf-250-38.local>
2015-03-26 13:12:53 -0500

    Small tweaks
    Committing before trying to fenestrate gulp modules.

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,38 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
-var minifycss = require('gulp-minify-css');
-var concat = require('gulp-concat');
-var plumber = require('gulp-plumber');
-var autoprefixer = require('gulp-autoprefixer');
-var minifycss = require('gulp-minify-css');
-var notify = require('gulp-notify');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var utility = require('gulp-util');
-var chalk = utility.colors;
+var gulp = 			require('gulp');
+var sass = 			require('gulp-sass');
+var browserSync = 	require('browser-sync');
+var minifycss = 	require('gulp-minify-css');
+var concat = 		require('gulp-concat');
+var plumber = 		require('gulp-plumber');
+var autoprefixer = 	require('gulp-autoprefixer');
+var minifycss = 	require('gulp-minify-css');
+var notify = 		require('gulp-notify');
+var uglify = 		require('gulp-uglify');
+var rename = 		require('gulp-rename');
+var utility = 		require('gulp-util');
+var chalk = 		utility.colors;
 
-// var spawn = require('child_process').spawn;
-// var p;
+//Config
 var paths = {
 	scss: ['./_partials/sass/*scss', './_partials/sass/**/*scss'],
 	css: './css',
 	js: ['./_partials/js/*js', './_partials/js/vendor/*js']
 };
 
+var autoprefixer_browsers = [
+	'> 1%',
+	'last 2 version',
+	'ff 3.5',
+	'ff >=16',
+	'ie >= 8',
+	'android >=2.3'
+];
 
-gulp.task('sass', function() {
-	var _error = function(err) {
+/* Extracts filenames from a path */
+function _filename(path) {
+	return path.substr(path.lastIndexOf('/') + 1);
+};
+function _error(err) {
 		console.log("Error:"+err);
 		notify.onError({
 			title:    "Gulp",
@@ -32,30 +42,24 @@ gulp.task('sass', function() {
 
 		this.emit('end');
 	};
-	var _filename = function(path) {
-		return path.substr(path.lastIndexOf('/') + 1);
-	};
 
-	debugger;
+gulp.task('sass', function() {
 
 	return gulp.src(paths.scss[0])
-		.pipe(plumber({errorHandler: _error}))
+		.pipe(plumber({errorHandler: _error})
 		.pipe(sass({
 			outputStyle: 'nested',
 			errLogToConsole: true,
-			onError: function(err){
-				notify({title: "Gulp", subtitle:"Error"}).write(err);
-				console.log(chalk.red("[Sass] ")+err);
-			},
+			onError: function(err){console.log(chalk.red("[Sass] ")+err);},
 			onSuccess: function(css){
 				var file = _filename(this.file);
 				var duration = this.result.stats.duration+"ms";
 				var _message = chalk.magenta("[Sass] ") +file + " Compiled in "+ duration;
-				notify({title: "Gulp", subtitle: "Success", message: _message });
 				console.log(_message);
+				browserSync.notify(file+"Compiled in "+duration, 10000);
 			}
 		}))
-		.pipe(autoprefixer('last 2 version', 'ff 3.5', 'ff >=16', 'ie >= 8', 'android >=2.3', '> 1%'))
+		.pipe(autoprefixer(autoprefixer_browsers))
 		.pipe(gulp.dest(paths.css));
 });
 
@@ -86,4 +90,6 @@ gulp.task('watch', function(){
 });
 
 
-gulp.task('default', ['browser-sync', 'watch']);
+gulp.task('default', ['browser-sync', 'watch'], function(){
+	debugger;
+});
