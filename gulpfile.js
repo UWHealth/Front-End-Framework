9ff49c9fcd5081bb8c845b3781d2ac2af1540d9b
Cleecanth <Chris@pa-wf-250-38.local>
2015-07-17 14:30:59 -0500

    Move to custom style guide generator
    Style guide generator now uses our own version on npm (called markdown-documentation-generator). No longer necessary to copy the index.js file over to the node module.
    **Make sure to run npm install**
    
    * Style guide now requires ```html_example when beginning code blocks, if you intend for that block to be rendered on the page.
      * This allows for code blocks that do not get rendered as html, making large code documentation much easier.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,10 @@
 var gulp		 = require('gulp');
 var sass		 = require('gulp-sass');
 var browserSync  = require('browser-sync');
-var minifycss	 = require('gulp-minify-css');
 var concat		 = require('gulp-concat');
 var plumber		 = require('gulp-plumber');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps   = require('gulp-sourcemaps');
-var minifycss    = require('gulp-minify-css');
 var notify       = require('gulp-notify');
 var uglify 		 = require('gulp-uglify');
 var rename 		 = require('gulp-rename');
@@ -18,6 +16,7 @@ var yarg		 = require('yargs').argv;
 var size 		 = require('gulp-size');
 var _if 		 = require('gulp-if');
 var jshint  	 = require('gulp-jshint');
+var replace 	 = require('gulp-replace');
 var reload		 = browserSync.reload;
 var chalk		 = utility.colors;
 
@@ -28,11 +27,11 @@ var paths = {
 	scss: ['./_partials/sass/*.scss', './_partials/sass/**/*.scss'],
 	css: './css',
 	js: ['./_partials/js/*.js', './_partials/js/vendor/*.js'],
-	kits: ['./_partials/kits/*.kit', '!./_partials/kits/_*.kit'],
+	kits: ['./_partials/kits/*.kit', '!./_partials/**/_*.kit'],
 	styleGuide: ['./styleguide/template.html', './styleguide/theme.css', './_partials/**/*.scss', './styleguide/*.scss']
 };
 
-var browserSyncWatch= ['./css/*.css', './js/**', './*.html', './styleguide/**'];
+var browserSyncWatch= ['./css/*.css', './js/**', './*.html', './styleguide/**', '!**.map'];
 
 var autoprefixer_browsers = [
 	'> 1%',
@@ -49,17 +48,22 @@ var autoprefixer_browsers = [
 
 var style_guide_output = true;
 var js_task = 'js';
-var sass_output = 'nested';
 var sass_task = 'sass';
+var sass_output = 'nested';
 var open_new_tab = false;
 var js_partials = paths.js;
 var source_maps = false;
 var minify = false;
 var lint = false;
+var dir = false;
 
 // console.log(yarg);
 // js_partials.push('!./_partials/js/_*.js');
 
+//Run gulp --ls to show directories instead of index.html in Browser Sync
+if (yarg.ls || yarg.dir){
+	dir = true;
+}
 
 //Run 'gulp --no-sg' if you don't want to compile the style guide
 if (yarg.sg === false || yarg.nosg){
@@ -108,7 +112,7 @@ function _error(err) {
  * --------------------------------*/
 
 //First task called when gulp is invoked
-gulp.task('default', ['watch', 'browser-sync']);
+gulp.task('default', ['watch', 'browser-sync', 'styleGuide']);
 
 //Watch file paths for changes (as defined in the paths variable)
 gulp.task('watch', function(){
@@ -129,9 +133,10 @@ gulp.task('browser-sync', [sass_task, js_task, 'kits'], function() {
     browserSync.init(browserSyncWatch, {
 		server: {
             baseDir: ['./'],
+			directory: dir
         },
 		ui:{
-			port: 3001
+			port: 3030
 		},
 		ghostMode: {
 			clicks: true,
@@ -161,6 +166,7 @@ gulp.task('sass', function() {
 		.pipe(sass({
 			outputStyle: sass_output,
 			errLogToConsole: true,
+			sourcemap: sourcemaps,
 			onError: function(err){
 				console.log(chalk.red("[Sass] ")+err);
 			},
@@ -177,6 +183,10 @@ gulp.task('sass', function() {
 		}))
 		.pipe(size({title: 'CSS', gzip: true, showFiles: true}))
 		.pipe(_if(source_maps, sourcemaps.write('./maps')))
+		//Remove Styleguide Comments
+		.pipe(_if(source_maps == false, replace(/(\/\* ?SG[\s\S]+?\*\/)/gmi, "")))
+		//Remove those new lines
+		.pipe(_if(source_maps == false, replace(/(\n{1,}(?=\n{2}))/gi, "")) )
 		.pipe(gulp.dest(paths.css))
 });
 
@@ -185,6 +195,7 @@ gulp.task('sass', function() {
 // Same as above, but with uglification
 
 gulp.task('js', function(){
+	// var filter = gFilter(['!**/_*.js']);
 
 	return gulp.src(js_partials)
 		.pipe(plumber({errorHandler: _error}))
@@ -204,15 +215,28 @@ gulp.task('js', function(){
 
 //Compile .kit files into html
 gulp.task('kits', function(){
-	gulp.src(paths.kits)
+	// var filter = gFilter(['**/_*.kit']);
+
+	return gulp.src(paths.kits)
 		.pipe(plumber({errorHandler: _error}))
 		.pipe(kit())
 		.pipe(gulp.dest('./'));
+});
+
+gulp.task('prod', ['switch-vars', sass_task, js_task, 'kits']);
+//Alias
+gulp.task('production', ['prod']);
+
+//Change variables for production
+gulp.task('switch-vars', function(){
+	sass_output = 'compressed';
+	minify = true;
+	lint = true;
 });
 
 //Compile style guide
 // use --no-sg argument to disable this
 gulp.task('styleGuide', function(){
 	gulp.src('./')
-		.pipe(shell('styleguide no-lf'));
+		.pipe(shell('md_documentation no-lf'));
 });
