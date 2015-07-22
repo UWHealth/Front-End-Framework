4986caa4081a5e307ea87ada882cd8f86c012a80
Cleecanth <Chris@pa-wf-250-38.local>
2015-07-22 12:43:44 -0500

    Improve landing page
    Added some deep links to the development guide and made tab examples show more of tabs.js flexibility.
    - Moved baseline toggle logic to page footer
    - Made first set of tabs behave like toggles with some custom animation thrown in as an example.

--- _partials/js/main.js
+++ _partials/js/main.js
@@ -1,9 +1,1 @@
-//Helper for hiding/showing baseline-grid
 
-$(function(){
-    $('#toggles').children().click(function(e){
-        var toggles = $(this).attr("id");
-        $('body').toggleClass(toggles);
-        e.preventDefault();
-    });
-});
--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-//Helper for hiding/showing baseline-grid
 
-$(function(){
-    $('#toggles').children().click(function(e){
-        var toggles = $(this).attr("id");
-        $('body').toggleClass(toggles);
-        e.preventDefault();
-    });
-});
 
 //# sourceMappingURL=maps/main.js.map
