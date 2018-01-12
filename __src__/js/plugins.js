1d0e9998caff0ec97d43a97a1bb9dce484dd18cf
Chris Lee <clee5@uwhealth.org>
2018-01-12 16:43:45 -0600

    Change ./_src to ./__src__, remove dead ./dist code

---
+++ __src__/js/plugins.js
@@ -0,0 +1,60 @@
+"use strict";
+
+//= include "vendor/velocity.js"
+//= include "vendor/velocity.ui.js"
+//= include "vendor/slick.js"
+//= include "vendor/mousetrap.js"
+//= include "vendor/stackblur.js"
+//= include "modules/jquerySelectors.js"
+//= include "modules/tables.js"
+//= include "modules/colors.js"
+//= include "modules/tabs.js"
+//= include "modules/toggle.js"
+
+// Avoid `console` errors in browsers that lack a console.
+
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+//Add keyboard shortcut for handling baseline debug class
+(function(){
+    Mousetrap.bind(["ctrl+'", "command+'"], function() {
+        if (document.body.className.indexOf('baseline-on') < 0) {
+            document.body.className+=' baseline-on';
+        }else {
+            document.body.className = document.body.className.replace('baseline-on', '');
+        }
+    })
+}());
+
+// Place any jQuery/helper plugins in here.
+
+$(function() {
+
+//Helper for hiding/showing baseline-grid
+    $('#js_baseline_toggle').click(function(e){
+        e.preventDefault();
+        var toggles = $(this).attr("id");
+        $('body').toggleClass('baseline-on');
+        e.preventDefault();
+    });
+});
