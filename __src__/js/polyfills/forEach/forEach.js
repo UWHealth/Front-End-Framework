62cdd08e5b5693b2c32326ef2fbb4ea7b858e5fe
cdl193 <clee5@uwhealth.org>
2018-01-29 20:01:46 -0600

    Rename __src__ to _src

--- __src__/js/polyfills/forEach/forEach.js
+++
@@ -1,7 +0,0 @@
-export default function (array, callback, scope) {
-    if (Array.isArray(array) && array !== 'undefined') {
-        for (var i = 0, arrLen = array.length; i < arrLen; i++) {
-            callback.call(scope, array[i], i, array); // passes back stuff we need
-        }
-    }
-};
---
+++ _src/js/polyfills/forEach/forEach.js
@@ -0,0 +1,7 @@
+export default function (array, callback, scope) {
+    if (array !== 'undefined' && array.length >= 0) {
+        for (var i = 0, arrLen = array.length; i < arrLen; i++) {
+            callback.call(scope, array[i], i, array); // passes back stuff we need
+        }
+    }
+};
