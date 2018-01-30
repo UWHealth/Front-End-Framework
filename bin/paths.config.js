99c661d82ec1f3030005a61cf34f40575745fa00
cdl193 <clee5@uwhealth.org>
2018-01-29 20:03:11 -0600

    Clean up paths so globs work on Windows
    Paths and globs are normalized -- globs always use POSIX, while others use native

--- bin/paths.config.js
+++ bin/paths.config.js
@@ -1,13 +1,15 @@
+/**
+ * @fileoverview - File paths imported by gulp and webpack
+**/
+
 const path = require('path');
 const process = require('process');
 const root = process.cwd();
+
 const bin  = __dirname;
-
-/* @fileoverview - File Paths imported by gulp and webpack */
-
-const dist = path.resolve(root, "dist/");
+const dist = path.resolve(root, "dist");
 const pub = path.join(dist, "public");
-const src  = path.resolve(root, "__src__");
+const src  = path.resolve(root, "_src");
 const docs = path.resolve(root, "docs");
 
 const PATHS = {
@@ -17,7 +19,7 @@ const PATHS = {
         "dist": dist,
         "pub": pub,
         "src": src,
-        "docs": docs,
+        "docs": docs
     }
 };
 
@@ -55,13 +57,14 @@ Object.assign(PATHS, {
         "entry": {
             "main": `${src}/sass/main.scss`,
             "print": `${src}/sass/print.scss`,
+            "components": `${src}/components/**/[!_]*.scss`,
             "styleguide": `${src}/styleguide/styleguide.scss`
         },
         "watch": {
+            "all": String.raw`./_src/**/*.scss`,
             "main": `${src}/sass/**/*.scss`,
             "styleguide": `${src}/styleguide/styleguide.scss`,
-            "components": `${src}/components/**/*.scss`,
-            "exclude": []
+            "components": `${src}/components/**/*.scss`
         },
         "dest": `${pub}/css`
     },
@@ -76,14 +79,13 @@ Object.assign(PATHS, {
         },
         "watch": {
             "main": `${src}/**/*.js`,
-            "samples": `${src}/__samples__/**/*.js`,
-            "exclude": []
+            "samples": `${src}/_samples/**/*.js`
         },
         "dest": `${pub}/js`
     },
     hbs: {
         "folders": {
-            "root": `${src}/components/`
+            "root": `${src}/components`
         },
         "watch": {
             "main": `${src}/components/**/*.hbs`,
@@ -91,74 +93,61 @@ Object.assign(PATHS, {
         },
         "entry": {
             "main": `${src}/components/**/*.hbs`,
-            "head": `${src}/components/head/_head.hbs`,
+            "header": `${src}/components/header/_header.hbs`,
             "footer": `${src}/components/footer/_footer.hbs`
         },
         "dest": `${pub}/components/`
     },
     samples: {
         "folders": {
-            "root": `${src}/__samples__/`
+            "root": `${src}/_samples/`
         },
         "entry": {
-            "base": `${src}/__samples__/base.hbs`,
-            "render": `${src}/js/dev/render-samples.js`
+            "base": `${src}/_samples/base.hbs`
         },
         "watch": {
-            "base": `${src}/__samples__/**/*.js`
+            "base": `${src}/_samples/**/*.js`
         },
         "dest": `${dist}/samples`
-    },
-    kits: {
-        "watch": {
-            "main": `${src}/**/*.kit`,
-            "exclude": []
-        },
-        "entry": {
-            "main": `${src}/**/*.kit`,
-            "exclude": [`!${src}/**/_*.kit`]
-        },
-        "dest": dist
     },
     styleGuide: {
         "entry": {
             "config": `${bin}/styleguide.config.js`,
             "templateFile": `${src}/styleguide/template.hbs`,
-            "themeFile": `${pub}/css/styleguide.min.css`,
+            "themeFile": `${pub}/css/styleguide.css`,
             "jquery": `${src}/styleguide/imports/jquery.js`,
             "toc": `${src}/styleguide/imports/toc.js`,
-            "jsonOutput": false,
             "htmlOutput": `${path.relative(root, pub)}/styleguide/index.html`,
         },
         "watch": {
             "imports": `${src}/styleguide/**/*.*`,
-            "style": `${pub}/**/styleguide.min.css`,
-            "config": `${bin}/styleguide.conf.js`,
-            "exclude": []
+            "style": `${pub}/css/styleguide.css`,
+            "config": `${bin}/styleguide.conf.js`
         },
         "dest": `${pub}/styleguide/index.html`
     },
     fonts: {
         "entry": {
-            "asap": `${src}/assets/fonts/Asap*.*`,
-            "open": `${src}/assets/fonts/opensans*.*`
+            "asap": `${src}/static/fonts/Asap*.*`,
+            "open": `${src}/static/fonts/opensans*.*`
         },
         "watch": {
-            "all": `${src}/assets/fonts/**/*.*`
+            "all": `${src}/static/fonts/**/*.*`
         },
         "dest": `${pub}/fonts`
     },
     images: {
         "entry": {
-            "main": `${src}/assets/img/**/*.(jpg|jpeg|png|gif)`,
-            "svg": `${src}/assets/img/svg/**/*.svg`
+            "all": `${src}/static/img/**/*.+(jpg|jpeg|png|gif|ico|svg)`,
+            "main": `${src}/static/img/**/*.+(jpg|jpeg|png|gif|ico)`,
+            "svg": `${src}/static/img/svg/**/*.svg`
         },
         "watch": {
-            "main": `${src}/assets/img/**/*.(jpg|jpeg|png|gif)`,
-            "jpeg": `${src}/assets/img/**/*.(jpg|jpeg)`,
-            "png": `${src}/assets/img/**/*.png`,
-            "gif": `${src}/assets/img/**/*.gif`,
-            "svg": `${src}/assets/img/svg/**/*.svg`
+            "all": `${src}/static/img/**/*.+(jpg|jpeg|png|gif|ico|svg)`,
+            "jpeg": `${src}/static/img/**/*.+(jpg|jpeg)`,
+            "png": `${src}/static/img/**/*.png`,
+            "gif": `${src}/static/img/**/*.gif`,
+            "svg": `${src}/static/img/svg/**/*.svg`
         },
         "dest": `${pub}/img`
     },
@@ -170,42 +159,55 @@ Object.assign(PATHS, {
         "watch": {
             "css": `${pub}/css/*.css`,
             "js": `${pub}/js/**/*.js`,
-            "samples": `${pub}/samples/**/*.html`,
-            "html": `*.html`,
-            "exclude": [`!**.map`]
+            "samples": `${dist}/samples/**/*.html`,
+            "exclude": [`!${dist}/**/*.map`]
         }
     }
 });
 
 
 /**
- * Adds "array" entry to path objects, allowing for a selection like `browsersync.watch.array`
+ * Adds "array" entry to path objects, allowing for a selection like `browsersync.watch.array`. Will convert
  * @return {Object} PATHS object with "array" keys added
  */
 const getPaths = function() {
     Object.keys(PATHS).forEach((target) => {
         Object.keys(PATHS[target]).forEach((group) => {
-            const currentGroup = PATHS[target][group];
+            let currentGroup = PATHS[target][group];
 
             if (typeof currentGroup === "object") {
                 let array = [];
 
                 Object.keys(currentGroup).forEach((file) => {
                     if (Array.isArray(currentGroup[file])) {
+                        currentGroup[file] = currentGroup[file].map(globNormalize);
                         array = array.concat(currentGroup[file]);
                     }
                     else {
+                        currentGroup[file] = globNormalize(currentGroup[file]);
                         array.push(currentGroup[file]);
                     }
                 });
 
                 currentGroup["array"] = array;
             }
+            else if (typeof currentGroup === 'string') {
+                currentGroup = globNormalize(currentGroup);
+            }
         });
     });
 
 
     return PATHS;
+};
+
+/**
+ * Normalize paths, including globs, replacing backslashes with forward slashes
+ * @param  {String} item path string
+ * @return {String}      normalize path string, if a glob, normalized to a POSIX path
+ */
+const globNormalize = function(item) {
+    return (item.indexOf("*") > -1) ? item.replace(/\\/g, '/') : path.normalize(item);
 };
 
 module.exports = getPaths();
