f3410fe54e443ea050ec38752bafccc32404c403
Chris Lee <clee5@uwhealth.org>
2018-02-08 17:59:26 -0600

    Demo generation cleanup

--- bin/tools/demo-router.js
+++ bin/tools/demo-router.js
@@ -1,26 +1,58 @@
 const path = require('path');
-const context = require.context('../../_src/components/', true, /\.demo\.js$/);
+//const context = require.context('../../_src/components/', true, /\.demo\.js$/);
+const glob = require('glob');
+const LOG = require('../tools/logger.js');
 
-const render = function(folder) {
-    // debugger;
-    // let bundle = require(`../../_src/components/${folder}/${folder}.demo.js`);
-    // debugger;
-    // return bundle();
-    return import(`../../_src/components/${folder}/${folder}.demo.js`);
+const context = glob.sync('./_src/components/**/*.demo.js');
+
+// console.log(__webpack_public_path__);
+
+const find = function(folder /* String */) {
+    debugger;
+    let bundle = require(`../../_src/components/${folder}/${folder}.demo.js`);
+    // console.log(bundle());
+    return bundle;
+    // return import(`../../_src/components/${folder}/${folder}.demo.js`);
 };
 
-module.exports = function(locals) {
-    const routes = {};
+const routes = {};
 
-    context.keys().forEach((file) => {
+
+module.exports = (locals) => {
+    //console.log("locals", locals.path);
+
+
+    context.forEach((file) => {
         const folder = path.basename(file, '.demo.js');
-        render(folder).then((template) => {
-            console.log(template);
-            routes['/' + folder] = template.default.render(locals);
-        }).catch((err) => {
-            console.log(err);
-        });
+        // __webpack_public_path__ = path.resolve(process.cwd(), 'dist/components/');
+
+        const bundle = find(folder);
+        if (bundle.default && bundle.default.render) {
+            routes['/' + folder + '/'] = bundle.default.render(locals);
+        }
+        else {
+            const message = 'The demo for "' + folder + '" doesn\'t export any html. Try using the Demo constructor.';
+            // routes['/' + folder + '/'] = `<html><body><p>${message}</p></body></html>`;
+            new LOG('Webpack Demo', message).error();
+        }
+        // console.log('bundle', bundle);
+
+        // routes['/' + folder + '/'] = bundle((template) => {
+        //     debugger;
+        //     // template(()=> {
+        //     //     debugger;
+        //     // });
+        //      return template.default.render;
+        //     // return routes['/' + folder] = ;
+        // }).catch((err) => {
+        //     console.log(err);
+        // });
     });
 
     return routes;
 };
+// module.exports = function(locals) {
+//     console.log(locals);
+//     return routes;
+//     // return routes[locals.path](locals);
+// };
--- bin/webpack/demos.webpack.config.js
+++ bin/webpack/demos.webpack.config.js
@@ -5,81 +5,76 @@
 const StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin');
 const webpack = require('webpack');
 
-const fs = require('fs');
 const glob = require('glob');
 const path = require('path');
 const cloneDeep = require('lodash.clonedeep');
 
-const PATHS = require('../paths.config.js');
-const WP_CONFIG = require('../../webpack.config.js');
+const PATHS = require("../paths.config.js");
+const WP_CONFIG = require("../../webpack.config.js");
+const DEMOS = glob.sync(PATHS.demos.entry.all);
 
 const config = cloneDeep(WP_CONFIG);
 
-config.context = __dirname;
+config.output.publicPath = "./dist/components/";
+config.output.filename = "[name].demo.js";
+config.output.chunkFilename = "[name].[id].demo.js";
 config.output.path = PATHS.demos.dest;
-config.output.publicPath = './dist/components/';
+config.resolve.modules = ["node_modules"];
+config.entry = {
+    index: path.resolve(__dirname, "../tools/demo-router.js"),
+    handlebars: "handlebars/runtime",
+};
+
+config.context = __dirname;
 config.target = "node";
 config.devtool = false;
-config.output.filename = '[name].demo.js';
-config.output.library = 'render';
+config.output.library = "render";
 config.output.libraryTarget = "umd";
-config.resolve.modules = ["node_modules", PATHS.demos.dest];
-config.entry = {
-    handlebars: 'handlebars/runtime',
-    index: path.resolve(__dirname, '../tools/demo-router.js'),
-};
 
 const staticPaths = [];
 
-const demos = glob.sync(PATHS.demos.entry.all);
+DEMOS.forEach((file) => {
+    const name = path.basename(file, ".demo.js");
+    staticPaths.push('/' + name + '/');
+});
 
-// fs.readdirSync(PATHS.demos.folders.root)
-//     .forEach((folder) => {
-//         const folderPath = path.resolve(PATHS.demos.folders.root, folder);
-//         fs.readdirSync(folderPath).forEach((file) => {
-//             if (file.indexOf('.demo.js') > -1) {
-//                 const name = path.basename(file, '.demo.js');
-//                 staticPaths.push(name + '/index.html');
-//             }
-//         });
-//     });
+// console.log(staticPaths);
 
 config.plugins.push(
     new StaticSiteGeneratorPlugin({
-        //paths: staticPaths,
-        entry: 'index',
+        paths: staticPaths,
+        entry: "index",
         locals: {
-            'variable': 'thing'
+            "variable": "thing"
         }
     }),
+    // Make handlebars its own chunk for quicker compilation
     new webpack.optimize.CommonsChunkPlugin({
         name: "handlebars",
         minChunks: Infinity,
-        async: 'handlebars.js'
+        async: "handlebars.js"
     }),
 
 );
 
 config.module.rules.push(
     {
+        // Allow for css to be inlined
         test: /\.demo\.css$/,
         use: 'raw-loader'
     },
     {
+        // Convert import() statements to regular node requires
+        // Allows handlebars templates to be loaded dynamically
         test: /\.demo\.js$/,
         use: [
-        // {
-        //     loader: 'bundle-loader',
-        //     options: {
-        //         lazy: true
-        //     }
-        // },
-        {
-            loader: 'babel-loader',
-            options: {
-                plugins: ["dynamic-import-node"]
+            {
+                loader: 'babel-loader',
+                options: {
+                    plugins: ["dynamic-import-node"]
+                }
             }
-        }]
+        ]
     }
 );
 
